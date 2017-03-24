a589d87f5257cd4125847757d381ec8fbf921950
Parag Dakle <paragdakle@users.noreply.github.com>
2017-03-23 20:07:04 -0500

    Fixed fines cleared bug

--- codebase/app/Http/Controllers/BorrowerController.php
+++ codebase/app/Http/Controllers/BorrowerController.php
@@ -116,7 +116,6 @@ class BorrowerController extends Controller
         if($borrower) {
             $book_loans = BookLoan::where('card_id', $borrower->card_id)->orderBy('date_out', 'desc')->get();
             $borrower->loan_history = $book_loans;
-            $this->updateFines();
             $fines = DB::table('fines as f')
                         ->join('book_loans as bl', 'f.loan_id', '=', 'bl.loan_id')
                         ->select('f.*', 'bl.isbn')
@@ -128,24 +127,6 @@ class BorrowerController extends Controller
         }
         else {
             return CommonMethods::generateErrorResponse(Config::get('errorcodes.USER_NOT_FOUND'), "User not found!");
-        }
-    }
-
-    private function updateFines() {
-        Fine::where('paid', 0)->delete();
-        $today_timestamp = strtotime('today');
-        $book_loans = BookLoan::whereNull('date_in')->where('due_date', '<', $today_timestamp)->get();
-        foreach($book_loans as $book_loan) {
-            $time_diff = $today_timestamp - $book_loan->due_date;
-            $hours = floor($time_diff / (60 * 60 * 24));
-            if($hours > 0) {
-                $fine_amount = $hours * Config::get('constants.fine_per_day');
-                $fine = new Fine;
-                $fine->loan_id = $book_loan->loan_id;
-                $fine->fine_amt = $fine_amount;
-                $fine->paid = false;
-                $fine->save();
-            }
         }
     }
 
